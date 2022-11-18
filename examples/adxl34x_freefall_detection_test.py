c195c3eb625b22a43dd7673224e5d4c5823af778
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:20 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/adxl34x_freefall_detection_test.py
+++ examples/adxl34x_freefall_detection_test.py
@@ -6,6 +6,7 @@ import board
 import adafruit_adxl34x
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
 # For ADXL343
 accelerometer = adafruit_adxl34x.ADXL343(i2c)
